eeb602268213a405b9f7667e637ff4de2f5b770d
macielexpedito <macielexpedito@gmail.com>
2026-05-29 22:11:38 -0300

    const

---
+++ script.js
@@ -0,0 +1,7 @@
+const conversorMoeda = document.querySelector("#moeda");
+const calculoIMC = document.querySelector("#imc");
+const conversorTemperatura = document.querySelector("#temperatura");
+const conversorVelocidade = document.querySelector("#velocidade");
+const conversorMassa = document.querySelector("#massa");
+const regraTres = document.querySelector("#regraDeTres");
+console.log (conversorMoeda);
