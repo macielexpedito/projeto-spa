84322c17c8406cbfa29fee62fd20f6c6ca7870b7
macielexpedito <macielexpedito@gmail.com>
2026-06-02 18:39:59 -0300

    alternar

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-const conversorMoeda = document.querySelector("#moeda");
-const calculoIMC = document.querySelector("#imc");
-const conversorTemperatura = document.querySelector("#temperatura");
-const conversorVelocidade = document.querySelector("#velocidade");
-const conversorMassa = document.querySelector("#massa");
-const regraTres = document.querySelector("#regraDeTres");
-console.log (conversorMoeda);
-const secoes = document.querySelectorAll("section");
 
-secoes.forEach((secao) => {
-    secao.style.display = "none";
+const elemento = {
+
+    aside: document.querySelector('aside'),
+    sections: document.querySelectorAll('section')
+
+}
+
+console.log(elemento.aside);
+
+elemento.aside.addEventListener('click', (e)=>{
+
+    e.preventDefault();
+
+    if(e.target.tagName === 'BUTTON'){
+
+        console.log(e.target.id);
+
+        displayController(e.target.id);
+
+    }
+
 });
+
+function displayController(id){
+
+    elemento.sections.forEach(section => {
+
+        section.classList.add('painel');
+
+        if(section.classList.contains(id)){
+
+            section.classList.remove('painel');
+
+        }
+
+    });
+
+}
+
 
